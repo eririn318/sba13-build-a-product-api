2ce9344d03e4effe291e3957d50f161fc91c6d4a
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2025-11-27 10:12:39 -0800

    final

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const {createProduct,getProductById,updatedProduct,deleteProduct,filterProduct,sortProduct,paginatedProduct,getAllProducts} = require('../controllers/productControllers')
+const express = require("express");
+const {
+  createProduct,
+  getProductById,
+  updatedProduct,
+  deleteProduct,
+  filterProduct,
+  sortProduct,
+  paginatedProduct,
+  getAllProducts,
+} = require("../controllers/productControllers");
 
-const router = express.Router()
+const router = express.Router();
 
-// // create
-router.post("/", createProduct)
+//create
+router.post("/", createProduct);
 
-
-// filter
-router.get('/filter', filterProduct)
+//filter
+router.get("/filter", filterProduct);
 
 //sort
-router.get('/sort', sortProduct)
+router.get("/sort", sortProduct);
 
-// pagination
-router.get('/pagination', paginatedProduct)
+//pagination
+router.get("/pagination", paginatedProduct);
 
-// // get all product
-router.get('/', getAllProducts)
+//get all product
+router.get("/", getAllProducts);
 
-// // get product by id
-router.get("/:id", getProductById)
+//get product by id
+router.get("/:id", getProductById);
 
-// // update
-router.put("/:id", updatedProduct)
+//update
+router.put("/:id", updatedProduct);
 
-// // delete
-router.delete('/:id', deleteProduct)
+//delete
+router.delete("/:id", deleteProduct);
 
-
-
-module.exports= router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,48 +1,23 @@
-// const express = require('express')
-// require('dotenv').config()
-// const morgan = require('morgan')
-// const {connectDB} = require('./db/connection')
-
-// // process.env.PORT to use in .env file
-// const PORT = 4000 || process.env.PORT
-// // to execute express
-// const app = express()
-
-// // Middleware
-// // We start seeing JSON(data)
-// app.use(express.json())
-// app.use(morgan("dev"))
-
-
-
-
-// app.listen(PORT, ()=>{
-//     console.log(`Server successfully running at port:${PORT}`)
-// })
-
-const express = require('express');
-const morgan = require('morgan')
-require('dotenv').config();
-const {connectDB} = require('./db/connection')
-const productRoutes = require('./routes/productRoutes')
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const { connectDB } = require("./db/connection");
+const productRoutes = require("./routes/productRoutes");
 
 const PORT = 4000 || process.env.PORT;
-connectDB()
+connectDB();
 const app = express();
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get('/', (req, res)=>{
-    res.send("We are connected to DataBase!")
-})
-// to connect to route
-app.use('/api/products', productRoutes)
-
-
+app.get("/", (req, res) => {
+  res.send("We are connected to DataBase!");
+});
+//to connect to route
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server successfully running at port: ${PORT}`);
-    
-})
+  console.log(`Server successfully running at port: ${PORT}`);
+});
